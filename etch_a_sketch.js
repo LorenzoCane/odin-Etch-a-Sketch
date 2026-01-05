56b2cb478097f35ca7b25e35138a3daf98db4143
LorenzoCane <lorenzo.cane985@edu.unito.it>
2026-01-05 16:39:03 +0100

    Implement basic level : draw, new grid and clear are working

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -4,16 +4,20 @@
 //----------------------------------------------
 //CONST & VARIABLES
 const log = console.log;
+
 const resMin = 1;
 const resMax = 100;
+
 let basicColor = "white";
 let resolution = 16;
 
+let opacity = false;
+let colorWrite = false;
 
 //QUERIES
 const mainContainer = document.querySelector(".mainContainer");
 const resBtn = document.querySelector("#resBtn");
-const wipeBtn = document.querySelector("#wipeBtn");
+const clearBtn = document.querySelector("#clearBtn");
 
 //----------------------------------------------
 //FUNCTIONS
@@ -31,7 +35,53 @@ function createSquare(){
 function createGrid(){
     for(let i = 0; i < resolution*resolution; i++){
         createSquare();
-    }
+    };
 }
+
+function getColor(){
+    if (!colorWrite){
+        return "black";
+    };
+
+    return "green"
+}
+
+function drawHover(elem){
+    if (elem.target.classList[0] == "square"){
+        let choosenColor = getColor();
+        elem.target.style.backgroundColor = choosenColor;
+    };
+}
+
+function createNewGrid(){
+    let newSize = Number(prompt("Let's begin something new! \nPlease select the dimension of the grid (1-100)"));
+    //Check
+    if (newSize < 1 || newSize > 100 || !Number.isInteger(newSize)){
+        alert("Please input a valid integer number between 1 and 100.")
+        return;
+    }
+
+    resolution = newSize; 
+
+    //Clear
+    mainContainer.replaceChildren();
+    //Create new grid
+    createGrid();
+
+}
+//----------------------------------------------
+//ACTIONS
+
+//New Grid button
+resBtn.addEventListener("click", createNewGrid);
+//Clear button
+clearBtn.addEventListener("click", () =>{
+    mainContainer.replaceChildren();
+    createGrid();
+});
+
+//Draw
+mainContainer.addEventListener("mouseover", drawHover);
+
 
 createGrid(); 
